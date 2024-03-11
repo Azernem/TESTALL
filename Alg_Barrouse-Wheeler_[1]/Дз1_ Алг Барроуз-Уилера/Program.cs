e0b9e6ab8a84f2749f23f186da7f6a6c928dd199
Azernem <160176623+Azernem@users.noreply.github.com>
2024-03-11 03:20:58 +0300

    Add files via upload

---
+++ Alg_Barrouse-Wheeler_[1]/Дз1_ Алг Барроуз-Уилера/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oruvnwpi23k
+{
+    internal class Program
+    {
+        static void Sorter(string[] mas)
+        {
+            for (int i = 0; i < mas.Length; ++i)
+            {
+                for (int j = 0; j < mas.Length - 1; ++j)
+                {
+                    if (String.Compare(mas[j], mas[j + 1]) > 0)
+                    {
+                        (mas[j], mas[j + 1]) = (mas[j + 1], mas[j]);
+
+                    }
+                }
+            }
+        }
+
+        static void BarrW()
+        {
+            string s = Console.ReadLine();
+            string[] mas = new string[s.Length];
+            for (int i = 0; i < s.Length; ++i)
+            {
+                mas[i] = s.Substring(i, s.Length - i);
+            }
+            Sorter(mas);
+            string s2 = "";
+            s = "$" + s;
+            for (int i = 0; i < mas.Length; ++i)
+            {
+                for (int z = 0; z < s.Length; ++z)
+                {
+                    if (mas[i] == s.Substring(z, s.Length - z))
+                    {
+                        s2 += s[z - 1];
+                    }
+                }
+            }
+            Console.WriteLine(s2 + " ");
+            Console.WriteLine(s2.IndexOf("$")); // Прямая
+
+            int[] ARR = new int[123];
+            for (int i = 0; i < s2.Length; ++i)
+            {
+                ARR[Convert.ToByte(s2[i])] += 1;
+            }
+
+            int[] a = new int[123];
+            int j = Convert.ToByte('$'); // если в качестве конца строки будет "$", а если нет, то заменяем его на s[-1]
+            a[j] = 1;
+            for (int i = j + 1; i < a.Length; ++i)
+            {
+                if (ARR[i] != 0)
+                {
+                    a[i] = a[j] + ARR[j];
+                    j = i;
+                }
+            }
+            int[] p = new int[s2.Length];
+            for (int i = 0; i < p.Length; ++i)
+            {
+                p[i] = a[Convert.ToByte(s2[i])];
+                a[Convert.ToByte(s2[i])] += 1;
+            }
+            string s3 = "$";
+            int k = s2.IndexOf("$");
+
+            for (int i = 0; i < p.Length; ++i)
+            {
+                p[i] -= 1;
+            }
+
+            for (int i = 0; i < s2.Length - 1; ++i)
+            {
+                s3 = s2[p[k]] + s3;
+                k = p[k];
+            }
+            Console.WriteLine(s3); // Обратная
+
+        }
+
+        static void Main(string[] args)
+        {
+            BarrW();
+        }
+    }
+}
---
+++ Bor_Trie/Bor_Trie/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bor_Trie
+{
+    public class Trie
+    {
+        public class Node
+        {
+            public bool Terminal { get; set; }
+            public Dictionary<char, Node> Children { get; set; }
+            public int Number { get; set; }
+            public Node()
+            {
+                Terminal = false;
+                Children = new Dictionary<char, Node>();
+                Number = 0;
+            }
+        }
+
+        public Node root;
+        public int Size { get; set; }
+        public Trie()
+        {
+            Size = 0;
+            root = new Node();
+        }
+        public bool Add(string s)
+        {
+            Node Chek_Node = root;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (Chek_Node.Children.ContainsKey(s[i]))
+                {
+                    Chek_Node = Chek_Node.Children[s[i]];
+                }
+                else
+                {
+                    Chek_Node.Children.Add(s[i], new Node());
+                    Size++;
+                    Chek_Node = Chek_Node.Children[s[i]];
+                }
+                Chek_Node.Number++;
+            }
+            if (Chek_Node.Terminal == false) { Chek_Node.Terminal = true; return true; }
+            return false;
+        }
+        public bool Contains(string s)
+        {
+            Node Chek_Node = root;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (Chek_Node.Children.ContainsKey(s[i]))
+                {
+                    Chek_Node = Chek_Node.Children[s[i]];
+                }
+                else { return false; }
+            }
+            return Chek_Node.Terminal;
+        }
+
+        public bool Remove(string s)
+        {
+            Node Chek_Node = root;
+            if (Contains(s))
+            {
+                for (int i = 0; i < s.Length; ++i)
+                {
+                    if (Chek_Node.Children[s[i]].Number > 1)
+                    {
+                        Chek_Node = Chek_Node.Children[s[i]];
+                        Chek_Node.Number--;
+                    }
+                    else { Chek_Node.Children.Remove(s[i]); Size -= (s.Length - i); return true; }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public int HowManyStartsWithPrefix(string prefix)
+        {
+            Node Chek_Node = root;
+            for (int i = 0; i < prefix.Length; ++i)
+            {
+                if (Chek_Node.Children.ContainsKey(prefix[i]))
+                {
+                    Chek_Node = Chek_Node.Children[prefix[i]];
+                }
+                else { return 0; }
+            }
+            return Chek_Node.Number;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); // вводим строки через пробел
+            string[] mas = s.Split(' ');
+            Trie Trie = new Trie();
+            foreach (string element in mas)
+            {
+                Console.WriteLine($"HowManyStartsWithPrefix:{Trie.HowManyStartsWithPrefix(element)},  Contains:{Trie.Contains(element)},  Remove:{Trie.Remove(element)}, Add:{Trie.Add(element)}");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+
+        }
+    }
+}
